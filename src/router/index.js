2e32e51dab49254c001f7f31bce7d6f5af093c4d
kiwi520 <roczhmg@163.com>
2018-02-22 17:33:54 +0800

    perfect swiper tabs function

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,6 @@ import Router from 'vue-router'
 
 import Home from '@/components/Home'
 import Video from '@/components/Video'
-import Roc from '@/components/Roc'
-import Rpc from '@/components/Rpc'
-import Usa from '@/components/Usa'
-import Cda from '@/components/Cda'
 import Page from '@/components/Page'
 
 Vue.use(Router)
@@ -22,12 +18,6 @@ export default new Router({
       path: '/video',
       name: 'Video',
       component: Video,
-      children:[
-         { path: '/video/roc', component: Roc},
-         { path: '/video/rpc', component: Rpc},
-         { path: '/video/usa', component: Usa},
-         { path: '/video/cda', component: Cda},
-      ]
     },
     {
       path: '/page',
